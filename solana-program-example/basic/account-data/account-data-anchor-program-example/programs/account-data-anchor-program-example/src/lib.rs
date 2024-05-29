6bfce67620eb65514e5e57b20ae069da91f4dcbc
jiasenwqbr <jasonjiasen@gmail.com>
2024-05-29 11:49:12 +0800

    account-data anchor rust code

--- solana-program-example/basic/account-data/account-data-anchor-program-example/programs/account-data-anchor-program-example/src/lib.rs
+++ solana-program-example/basic/account-data/account-data-anchor-program-example/programs/account-data-anchor-program-example/src/lib.rs
@@ -1,15 +1,24 @@
+#![allow(clippy::result_large_err)]
 use anchor_lang::prelude::*;
+use instructions::*;
+pub mod instructions;
+pub mod state;
 
 declare_id!("hB93vXZbZQ42Zczn2joFoVtmVw3ZG87wLrVDtCkiTYP");
 
 #[program]
 pub mod account_data_anchor_program_example {
+    use instructions::create;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn anchor_program_example(
+        ctx: Context<CreateAddressInfo>,
+        name: String,
+        house_number: u8,
+        city: String,
+        street: String,
+    ) -> Result<()> {
+        create::create_address_info(ctx, name, house_number, street, city)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
